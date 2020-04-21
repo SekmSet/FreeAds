3a81669c9e1accd1eb907055e3f11bbc6b2d13fc
Priscilla Joly <priscilla.joly@epitech.eu>
2020-04-21 14:02:10 +0200

    matching day4

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
+use App\Color;
 use App\Http\Requests\EditUserRequest;
+use App\Theme;
+use App\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
@@ -14,6 +18,24 @@ class IndexController extends Controller
     }
 
     public function indexAction(){
-        return view('home');
+        $users = Auth::user();
+
+        if ($users){
+            $byCity  =  Article::where('city',$users->city)->get();
+            $byTheme =  Article::where('theme_id',$users->theme_id)->get();
+            $byColor =  Article::where('color_id',$users->color_id)->get();
+
+            return view('home', [
+                'byCity' => $byCity,
+                'byTheme' => $byTheme,
+                'byColor' => $byColor
+            ]);
+        }
+
+        return view('home', [
+            'byCity' => [],
+            'byTheme' => [],
+            'byColor' => []
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,21 @@
                 <div class="card-header"><h2>Nos suggestions conçu spécialement pour vous …</h2>
             </div>
             <div class="card-body">
+                <h3>Suggestions par vile …</h3>
 
-            </div>
+                @foreach ($byCity as $city)
+                    <p>{{$city->title}}</p>
+                @endforeach
+                <hr>
+                <h3>Suggestions par thème …</h3>
+                @foreach ($byTheme as $theme)
+                    <p>{{$theme->title}}</p>
+                @endforeach
+                <hr>
+                <h3>Suggestions par couleur …</h3>
+                @foreach ($byColor as $color)
+                    <p>{{$color->title}}</p>
+                @endforeach
             </div>
         </div>
     </div>
