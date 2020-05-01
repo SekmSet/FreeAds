389ab76acac4e5b11b27e7906188aaa519fd8f4d
Priscilla JOLY <57559018+SekmSet@users.noreply.github.com>
2020-05-01 12:25:30 +0200

    Fix #2
    Fix #2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Fix for MySQL < 5.7.7 and MariaDB < 10.2.2
+        // https://laravel.com/docs/master/migrations#creating-indexes
+        Schema::defaultStringLength(191);
+        
         view()->composer('*', function(View $view) {
             $messages = Message::where('repeater_id', Auth::id())->whereNull('read_at');
             $view->with('counter_message', $messages->count());
